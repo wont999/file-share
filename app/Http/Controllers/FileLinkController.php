19d6bdda1ec1b9d71c4dc31c3c19e4cb81ebe3e7
Plotnikov Dmitriy <horvist@yandex.ru>
2025-04-21 18:11:23 +0300

    чистка

--- app/Http/Controllers/FileLinkController.php
+++ app/Http/Controllers/FileLinkController.php
@@ -70,18 +70,11 @@ class FileLinkController extends Controller
             ->with('error', 'Ссылка недействительна или не существует');
         }
         if ($link->is_used) {
-            // return response()->json([
-            //     'message' => 'Ссылка уже использована',
-            // ]);
             return redirect()->route('file.download', ['token' => $token])
             ->with('error', 'Эта ссылка уже была использована');
 
         }
         if (!Hash::check( $request->password, $link->password)) {
-            // return response()->json([
-            //     'message' => 'Неверный пароль'
-            // ], 403);
-
             return redirect()->route('file.download', ['token' => $token])
                 ->withErrors(['password' => 'Неверный пароль'])
                 ->withInput();
@@ -89,10 +82,6 @@ class FileLinkController extends Controller
 
         $file = $link->file;
         if (!Storage::disk('local')->exists($file->path)) {
-            // return response()->json([
-            //     'message' => 'Файл не найден на сервере'
-            // ], 404);
-            //return redirect()->back()->with('error', 'Файл не найден на сервере');
             return redirect()->route('file.download', ['token' => $token])
             ->with('error', 'Файл не найден на сервере');
         }
